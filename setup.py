a50760d5e2e67b569c46d24811b1e7fc93472898
Emmanuel Arias <emmanuelarias30@gmail.com>
2018-09-02 12:54:02 -0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ConfigFilesManager',
+    version='0.1',
+    packages=['ConfigFilesManager'],
+    url='https://github.com/orgs/PyModuleManage',
+    license='GPL-v3+',
+    author='eamanu',
+    author_email='eamanu@eamanu.com',
+    description='A python module to a easy manage configs files for applications'
+)
